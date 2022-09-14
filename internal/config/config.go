453856aa922ea0d774c464e07c5d5ce60684c894
Schattenbrot <aaron.machill@web.de>
2022-09-14 16:34:34 +0200

    fixed an issue where some configurations wasn't properly converted to app configurations

--- internal/config/config.go
+++ internal/config/config.go
@@ -43,19 +43,19 @@ func Init() AppConfig {
 
 	var jwt string
 	flag.StringVar(&jwt, "jwt", "wonderfulsecretphrase", "the jwt token secret")
-	app.Config.JWT = []byte(jwt)
 
 	var cors string
 	flag.StringVar(&cors, "cors", "http://* https://*", "the by cors allowed origins")
-	app.Config.Cors = strings.Split(cors, " ")
 
 	var cookieSecret string
 	flag.StringVar(&cookieSecret, "cookieSecret", "verywellkeptsecretphrase", "the cookie token secret")
-	app.Config.Cookie.Secret = []byte(cookieSecret)
 	flag.StringVar(&app.Config.Cookie.Name, "cookieName", "basic-login-api-gin", "the name of the cookie")
 	flag.BoolVar(&app.Config.Cookie.Secure, "cookieSameSite", false, "same site policy of cookies")
 
 	flag.Parse()
+	app.Config.JWT = []byte(jwt)
+	app.Config.Cors = strings.Split(cors, " ")
+	app.Config.Cookie.Secret = []byte(cookieSecret)
 
 	app.ServerStartTime = time.Now()
 	app.Version = "1.0.0"
